e39c7be4ae9bb31a4aeb1a2a204fc60af2bc4f5c
zian-2002 <zianalfina223@gmail.com>
2025-10-23 15:51:54 +0700

    Update title and footer in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Toko Skincare </title>
+    <title>Toko Skincare</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
@@ -14,7 +14,6 @@
             background-color: #ff6fa5;
             padding: 15px 0;
             color: white;
-            text-align: center;
         }
         footer {
             background-color: #ff6fa5;
@@ -28,9 +27,6 @@
         main {
             padding: 40px;
             min-height: 70vh;
-        }
-        nav {
-            background-color: #ff6fa5;
         }
         .nav-link {
             color: white !important;
@@ -46,12 +42,15 @@
     <header>
         <div class="container-fluid">
             <div class="d-flex justify-content-between align-items-center px-4">
-                <h4 class="m-0 fw-bold text-white"> Skincare </h4>
-                <nav>
-                    <a href="{{ route('dashboard', ['username' => $username ?? request('username')]) }}" class="nav-link d-inline-block mx-2">Dashboard</a>
-                    <a href="{{ route('profile', ['username' => $username ?? request('username')]) }}" class="nav-link d-inline-block mx-2">Profile</a>
-                    <a href="{{ route('pengelolaan', ['username' => $username ?? request('username')]) }}" class="nav-link d-inline-block mx-2">Pengelolaan</a>
-                </nav>
+                <h4 class="m-0 fw-bold text-white">Toko Skincare</h4>
+
+                @if (!request()->is('/'))
+                    <nav>
+                        <a href="{{ route('dashboard', ['username' => $username ?? request('username')]) }}" class="nav-link d-inline-block mx-2">Dashboard</a>
+                        <a href="{{ route('profile', ['username' => $username ?? request('username')]) }}" class="nav-link d-inline-block mx-2">Profile</a>
+                        <a href="{{ route('pengelolaan', ['username' => $username ?? request('username')]) }}" class="nav-link d-inline-block mx-2">Pengelolaan</a>
+                    </nav>
+                @endif
             </div>
         </div>
     </header>
@@ -61,7 +60,7 @@
     </main>
 
     <footer>
-        © Toko Skincare Jezi | UTS Pemrograman Web
+        © Toko Skincare Jezi | UTS Pemrograman Web 2025
     </footer>
 
 </body>
